842cf00b037cab449acafb3a3504667acafd6801
Cian Mackle <cian@highwaythreesolutions.com>
2019-02-22 15:18:36 -0800

    Updates

--- precache-manifest.4352e60f4a9a23bba43c48f4988c2a4c.js
+++ precache-manifest.4352e60f4a9a23bba43c48f4988c2a4c.js
@@ -4,32 +4,32 @@ self.__precacheManifest = [
     "url": "/glut-in/static/media/not-detected-logo.c59074f5.png"
   },
   {
-    "revision": "cbf81714251b32ab9c71",
-    "url": "/glut-in/static/css/main.628187f8.chunk.css"
+    "revision": "34f93885fd32ff2d24c9",
+    "url": "/glut-in/static/css/main.73caad8e.chunk.css"
   },
   {
-    "revision": "01177c27078cb792e16b",
-    "url": "/glut-in/static/js/1.01177c27.chunk.js"
+    "revision": "da23b5a6853fe02cfc57",
+    "url": "/glut-in/static/js/runtime~main.da23b5a6.js"
   },
   {
-    "revision": "701c805d5c777644fa75",
-    "url": "/glut-in/static/js/runtime~main.701c805d.js"
+    "revision": "85c86ee38e46b7295464",
+    "url": "/glut-in/static/js/2.85c86ee3.chunk.js"
+  },
+  {
+    "revision": "bae7d1a0d7c89217670741b39e62e9ad",
+    "url": "/glut-in/static/media/camera-button.bae7d1a0.png"
   },
   {
     "revision": "506c846118ee3fcaf87900ba0b90f00f",
     "url": "/glut-in/static/media/close.506c8461.svg"
   },
   {
-    "revision": "efabf30606d9a15acd49e9413775d1f2",
-    "url": "/glut-in/static/media/hamburger.efabf306.svg"
-  },
-  {
     "revision": "44cfcd7b992105e847d7c71c4ef22430",
     "url": "/glut-in/static/media/detected-logo.44cfcd7b.png"
   },
   {
-    "revision": "cbf81714251b32ab9c71",
-    "url": "/glut-in/static/js/main.cbf81714.chunk.js"
+    "revision": "34f93885fd32ff2d24c9",
+    "url": "/glut-in/static/js/main.34f93885.chunk.js"
   },
   {
     "revision": "ee8704209f0cafbd641a27bedda73cc5",
@@ -52,7 +52,7 @@ self.__precacheManifest = [
     "url": "/glut-in/static/media/label-gluten-free-2.863a2528.jpg"
   },
   {
-    "revision": "139da19fea7557e2c57290a37a24b3b7",
+    "revision": "006c8eef867fcb9b9618a8455313785a",
     "url": "/glut-in/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/glut-in/precache-manifest.535d112d129240b5ee3a71067789d27c.js"
+  "/glut-in/precache-manifest.4352e60f4a9a23bba43c48f4988c2a4c.js"
 );
 
 workbox.clientsClaim();
